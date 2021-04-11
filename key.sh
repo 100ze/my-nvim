bda021fec161a60de1caf9914e364f9b0b470bd1
wilton ribeiro <wiltonribeiromaster@gmail.com>
2021-04-11 14:20:35 -0300

    alterações para o arch

---
+++ key.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+sudo localectl set-x11-keymap br
---
+++ screen.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+#startx /bin/i3
+xrandr --output LVDS-1 --off 
+#sudo setxkbmap -model abnt2 -layout br -variant abnt2
+#nitrogen --set-scaled --save ~/Wall/665374.jpg
